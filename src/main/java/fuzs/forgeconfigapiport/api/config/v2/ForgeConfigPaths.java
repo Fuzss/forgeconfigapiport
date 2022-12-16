1962191412972b5e7873604dd766268efa0d6fe0
Fuzss <28218241+Fuzss@users.noreply.github.com>
2022-12-16 20:02:19 +0100

    fix forgeconfigapiport config loading issue

--- src/main/java/fuzs/forgeconfigapiport/api/config/v2/ForgeConfigPaths.java
+++ src/main/java/fuzs/forgeconfigapiport/api/config/v2/ForgeConfigPaths.java
@@ -30,7 +30,8 @@ public interface ForgeConfigPaths {
 
     /**
      * The directory where server configs are stored at. By default, this is inside the world directory instead of the common <code>.minecraft/config/</code> directory.
-     * <p>Forge Config Api Port has a config setting (which is not present in Forge itself) to load server configs from the common directory instead. This method will always provide the directory that is currently set via the Forge Config Api Port config.
+     * <p>Forge Config Api Port has a config setting (which is not present in Forge itself) to load server configs from the common directory instead.
+     * This method will always provide the directory that is currently set via the Forge Config Api Port config.
      *
      * @param server the current {@link MinecraftServer}
      * @return server config path
@@ -39,7 +40,7 @@ public interface ForgeConfigPaths {
 
     /**
      * Path where default configs are stored, by default inside of <code>.minecraft/defaultconfigs</code>.
-     * <p>This path is configurable via the Forge Config Api Port config. The config is loaded during the <code>preLaunch</code> phase of Fabric Loader, it might therefore not be available in some early mixins.
+     * <p>This path is configurable via the Forge Config Api Port config.
      * <p>Default configs are mainly intended for server configs to allow pre-configured configs to be applied to every
      * newly created world (since server configs are handled per world). This mechanic also applies to other config types (although they are only created once).
      *
