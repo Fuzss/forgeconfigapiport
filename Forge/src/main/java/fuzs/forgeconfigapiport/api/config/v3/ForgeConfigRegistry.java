eedc65f8cb496e3be37781d884f97c6e680343ed
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-12-03 20:58:57 +0100

    forge related updates

--- Forge/src/main/java/fuzs/forgeconfigapiport/api/config/v3/ForgeConfigRegistry.java
+++ Forge/src/main/java/fuzs/forgeconfigapiport/api/config/v3/ForgeConfigRegistry.java
@@ -6,7 +6,7 @@ import net.neoforged.fml.config.IConfigSpec;
 
 /**
  * Registry for adding your configs. Opposed to Fabric/Quilt this registry serves as a bridge between NeoForge's and Forge's config systems.
- * NeoForge configs supplied by Forge Config Api Port are automatically adapted as Forge configs. Therefore, the returned {@link ModConfig} instance is the original Forge mod config.
+ * NeoForge configs supplied by Forge Config Api Port are automatically adapted as Forge configs. Therefore, the returned {@link ModConfig} instance is the original Forge mod config, and not a class from Forge Config Api Port.
  * <p>As configs are added to Forge's native config system, original restrictions apply, and none of the changes implemented by Forge Config Api Port on Fabric/Quilt hold,
  * like loading client and common configs immediately upon registration as well as loading server configs from the global <code>.minecraft/config</code> directory.
  */
@@ -17,25 +17,25 @@ public interface ForgeConfigRegistry {
     ForgeConfigRegistry INSTANCE = new ForgeConfigRegistryImpl();
 
     /**
-     * Register a new mod config, only difference from registering on Forge is <code>modId</code> has to be provided as there is no loading context to get that information from
+     * Register a new mod config. Call this method only during {@link FMLConstructModEvent}, as would be done for Forge's configs; not during any of the (sided) setup events.
      *
      * @param modId mod id of your mod
      * @param type  type of this mod config (client, common, or server)
      * @param spec  the built config spec
-     * @return the {@link ModConfig} instance
+     * @return the Forge {@link ModConfig} instance
      *
      * @throws IllegalArgumentException when no mod container is found for <code>modId</code>
      */
     ModConfig register(String modId, ModConfig.Type type, IConfigSpec<?> spec);
 
     /**
-     * Register a new mod config, only difference from registering on Forge is <code>modId</code> has to be provided as there is no loading context to get that information from
+     * Register a new mod config. Call this method only during {@link FMLConstructModEvent}, as would be done for Forge's configs; not during any of the (sided) setup events.
      *
      * @param modId    mod id of your mod
      * @param type     type of this mod config (client, common, or server)
      * @param spec     the built config spec
      * @param fileName file name to use instead of default
-     * @return the {@link ModConfig} instance
+     * @return the Forge {@link ModConfig} instance
      *
      * @throws IllegalArgumentException when no mod container is found for <code>modId</code>
      */
