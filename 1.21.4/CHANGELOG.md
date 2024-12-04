42d138793195b767fd462131817028859668406f
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-12-04 15:14:49 +0100

    prepare 1.21.4 port

---
+++ 1.21.4/CHANGELOG.md
@@ -0,0 +1,17 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [v21.3.2-1.21.3] - 2024-11-14
+### Fixed
+- Attempt fixing being unable to manually save Forge configs via `ForgeConfigSpec::save`
+
+## [v21.3.1-1.21.3] - 2024-10-27
+- Publish mod for Minecraft Forge
+
+## [v21.3.0-1.21.3] - 2024-10-24
+- Update to Minecraft 1.21.3
+
+[Keep a Changelog]: https://keepachangelog.com/en/1.0.0/
---
+++ 1.21.4/Fabric/src/main/java/fuzs/forgeconfigapiport/fabric/impl/ForgeConfigAPIPortFabric.java
@@ -0,0 +1,75 @@
+package fuzs.forgeconfigapiport.fabric.impl;
+
+import fuzs.forgeconfigapiport.fabric.api.forge.v4.ForgeConfigRegistry;
+import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.NeoForgeConfigRegistry;
+import fuzs.forgeconfigapiport.fabric.impl.handler.ServerLifecycleHandler;
+import fuzs.forgeconfigapiport.fabric.impl.network.configuration.SyncConfig;
+import fuzs.forgeconfigapiport.fabric.impl.network.payload.ConfigFilePayload;
+import fuzs.forgeconfigapiport.impl.ForgeConfigAPIPort;
+import fuzs.forgeconfigapiport.impl.services.CommonAbstractions;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.fabricmc.fabric.api.networking.v1.ServerConfigurationConnectionEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerConfigurationNetworking;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerConfigurationPacketListenerImpl;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.config.ModConfigs;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+public class ForgeConfigAPIPortFabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        registerMessages();
+        registerEventHandlers();
+        setupDevelopmentEnvironment();
+    }
+
+    private static void registerMessages() {
+        ServerConfigurationConnectionEvents.CONFIGURE.register(
+                (ServerConfigurationPacketListenerImpl handler, MinecraftServer server) -> {
+                    if (ServerConfigurationNetworking.canSend(handler, ConfigFilePayload.TYPE)) {
+                        handler.addTask(new SyncConfig(handler));
+                    }
+                });
+        PayloadTypeRegistry.configurationS2C().register(ConfigFilePayload.TYPE, ConfigFilePayload.STREAM_CODEC);
+    }
+
+    private static void registerEventHandlers() {
+        ServerLifecycleEvents.SERVER_STARTING.addPhaseOrdering(ServerLifecycleHandler.BEFORE_PHASE,
+                Event.DEFAULT_PHASE
+        );
+        ServerLifecycleEvents.SERVER_STARTING.register(ServerLifecycleHandler.BEFORE_PHASE,
+                ServerLifecycleHandler::onServerStarting
+        );
+        ServerLifecycleEvents.SERVER_STOPPED.addPhaseOrdering(Event.DEFAULT_PHASE, ServerLifecycleHandler.AFTER_PHASE);
+        ServerLifecycleEvents.SERVER_STOPPED.register(ServerLifecycleHandler.AFTER_PHASE,
+                ServerLifecycleHandler::onServerStopped
+        );
+        ServerLifecycleEvents.SERVER_STOPPING.register((MinecraftServer server) -> {
+            // Reset WORLD type config caches
+            ModConfigs.getFileMap().values().forEach(config -> {
+                if (config.getSpec() instanceof ModConfigSpec spec) {
+                    spec.resetCaches(ModConfigSpec.RestartType.WORLD);
+                }
+            });
+        });
+    }
+
+    private static void setupDevelopmentEnvironment() {
+        if (CommonAbstractions.INSTANCE.includeTestConfigs()) {
+            NeoForgeConfigRegistry.INSTANCE.register(ForgeConfigAPIPort.MOD_ID, ModConfig.Type.SERVER,
+                    new ModConfigSpec.Builder().comment("hello world").define("dummy_entry", true).next().build(),
+                    "forgeconfigapiport-server-neoforge.toml"
+            );
+            ForgeConfigRegistry.INSTANCE.register(ForgeConfigAPIPort.MOD_ID, ModConfig.Type.SERVER,
+                    new ForgeConfigSpec.Builder().comment("hello world").define("dummy_entry", true).next().build(),
+                    "forgeconfigapiport-server-forge.toml"
+            );
+        }
+    }
+}
