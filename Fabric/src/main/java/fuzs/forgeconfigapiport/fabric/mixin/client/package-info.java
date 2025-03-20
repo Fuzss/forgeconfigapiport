faa148d2e50a65f12330a5e63cc50fe79e0f0e19
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-03-20 12:21:15 +0100

    remove more unsupported versions

--- Fabric/src/main/java/fuzs/forgeconfigapiport/fabric/mixin/client/package-info.java
+++
@@ -1,4 +0,0 @@
-@ApiStatus.Internal
-package fuzs.forgeconfigapiport.fabric.mixin.client;
-
-import org.jetbrains.annotations.ApiStatus;
--- Forge/src/main/java/fuzs/forgeconfigapiport/forge/impl/package-info.java
+++
@@ -1,4 +0,0 @@
-@ApiStatus.Internal
-package fuzs.forgeconfigapiport.forge.impl;
-
-import org.jetbrains.annotations.ApiStatus;
