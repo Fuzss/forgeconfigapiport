b4436e4b6dc3a83dd520d3d936e9d4576dccf473
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-03-26 11:55:16 +0100

    make fabric api work

--- Fabric/src/main/java/fuzs/forgeconfigapiport/fabric/impl/client/ForgeConfigAPIPortFabricClient.java
+++ Fabric/src/main/java/fuzs/forgeconfigapiport/fabric/impl/client/ForgeConfigAPIPortFabricClient.java
@@ -1,12 +1,9 @@
 package fuzs.forgeconfigapiport.fabric.impl.client;
 
 import com.mojang.brigadier.CommandDispatcher;
-import fuzs.forgeconfigapiport.fabric.api.v5.client.ConfigScreenFactoryRegistry;
-import fuzs.forgeconfigapiport.fabric.impl.client.commands.FabricConfigCommand;
+import fuzs.forgeconfigapiport.fabric.impl.client.commands.ClientConfigCommand;
 import fuzs.forgeconfigapiport.fabric.impl.network.ConfigSync;
 import fuzs.forgeconfigapiport.fabric.impl.network.payload.ConfigFilePayload;
-import fuzs.forgeconfigapiport.impl.ForgeConfigAPIPort;
-import fuzs.forgeconfigapiport.impl.services.CommonAbstractions;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
@@ -18,7 +15,6 @@ import net.minecraft.client.multiplayer.ClientConfigurationPacketListenerImpl;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.commands.CommandBuildContext;
 import net.neoforged.fml.config.ModConfigs;
-import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
 public class ForgeConfigAPIPortFabricClient implements ClientModInitializer {
@@ -27,18 +23,18 @@ public class ForgeConfigAPIPortFabricClient implements ClientModInitializer {
     public void onInitializeClient() {
         registerMessages();
         registerEventHandlers();
-        setupDevelopmentEnvironment();
     }
 
     private static void registerMessages() {
-        ClientConfigurationNetworking.registerGlobalReceiver(ConfigFilePayload.TYPE, (ConfigFilePayload payload, ClientConfigurationNetworking.Context context) -> {
-            ConfigSync.receiveSyncedConfig(payload.contents(), payload.fileName());
-        });
+        ClientConfigurationNetworking.registerGlobalReceiver(ConfigFilePayload.TYPE,
+                (ConfigFilePayload payload, ClientConfigurationNetworking.Context context) -> {
+                    ConfigSync.receiveSyncedConfig(payload.contents(), payload.fileName());
+                });
     }
 
     private static void registerEventHandlers() {
-        ClientCommandRegistrationCallback.EVENT.register((CommandDispatcher<FabricClientCommandSource> dispatcher, CommandBuildContext registryAccess) -> {
-            FabricConfigCommand.register(dispatcher, FabricClientCommandSource::sendFeedback);
+        ClientCommandRegistrationCallback.EVENT.register((CommandDispatcher<FabricClientCommandSource> commandDispatcher, CommandBuildContext commandBuildContext) -> {
+            ClientConfigCommand.register(commandDispatcher);
         });
         ClientConfigurationConnectionEvents.COMPLETE.register((ClientConfigurationPacketListenerImpl handler, Minecraft client) -> {
             ConfigSync.handleClientLoginSuccess();
@@ -51,11 +47,5 @@ public class ForgeConfigAPIPortFabricClient implements ClientModInitializer {
                 }
             });
         });
-    }
-
-    private static void setupDevelopmentEnvironment() {
-        if (CommonAbstractions.INSTANCE.isDevelopmentEnvironment(ForgeConfigAPIPort.MOD_ID)) {
-            ConfigScreenFactoryRegistry.INSTANCE.register(ForgeConfigAPIPort.MOD_ID, ConfigurationScreen::new);
-        }
     }
 }
--- Fabric/src/main/java/fuzs/forgeconfigapiport/fabric/impl/client/commands/arguments/ModIdArgument.java
+++ Fabric/src/main/java/fuzs/forgeconfigapiport/fabric/impl/client/commands/arguments/ModIdArgument.java
@@ -1,34 +1,50 @@
 package fuzs.forgeconfigapiport.fabric.impl.client.commands.arguments;
 
+import com.google.common.base.Predicates;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.commands.SharedSuggestionProvider;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Predicate;
 
 public class ModIdArgument implements ArgumentType<String> {
     private static final List<String> EXAMPLES = Arrays.asList("fabric", "modmenu");
 
+    private final Predicate<String> filter;
+
+    private ModIdArgument(Predicate<String> filter) {
+        this.filter = filter;
+    }
+
     public static ModIdArgument modIdArgument() {
-        return new ModIdArgument();
+        return new ModIdArgument(Predicates.alwaysTrue());
+    }
+
+    public static ModIdArgument modIdArgument(Predicate<String> filter) {
+        return new ModIdArgument(filter);
     }
 
     @Override
-    public String parse(final StringReader reader) throws CommandSyntaxException {
+    public String parse(final StringReader reader) {
         return reader.readUnquotedString();
     }
 
     @Override
     public <S> CompletableFuture<Suggestions> listSuggestions(final CommandContext<S> context, final SuggestionsBuilder builder) {
-        return SharedSuggestionProvider.suggest(FabricLoader.getInstance().getAllMods().stream().map(container -> container.getMetadata().getId()), builder);
+        return SharedSuggestionProvider.suggest(FabricLoader.getInstance()
+                .getAllMods()
+                .stream()
+                .map((ModContainer modContainer) -> modContainer.getMetadata().getId())
+                .filter(this.filter), builder);
     }
 
     @Override
