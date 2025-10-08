435bedb86624e4be4569579647b80574de759bf4
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-08 10:21:41 +0200

    prepare 1.21.10 port

--- 1.21.9/Fabric/src/main/java/fuzs/forgeconfigapiport/fabric/impl/client/core/package-info.java
+++
@@ -1,4 +0,0 @@
-@ApiStatus.Internal
-package fuzs.forgeconfigapiport.fabric.impl.client.core;
-
-import org.jetbrains.annotations.ApiStatus;
--- 1.21.9/Fabric/src/main/java/fuzs/forgeconfigapiport/fabric/impl/package-info.java
+++
@@ -1,4 +0,0 @@
-@ApiStatus.Internal
-package fuzs.forgeconfigapiport.fabric.impl;
-
-import org.jetbrains.annotations.ApiStatus;
