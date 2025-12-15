e78e07380844d4c573f947347402cfe3830de511
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-12-15 18:40:22 +0100

    enable minecraft forge

--- 1.21.11/Forge/build.gradle.kts
+++ 1.21.11/Forge/build.gradle.kts
@@ -1,12 +1,10 @@
-import commonProject
-import expectPlatform
 import fuzs.multiloader.metadata.ModLoaderProvider
 import fuzs.multiloader.neoforge.setupModsTomlTask
+import fuzs.multiloader.neoforge.toml.NeoForgeModsTomlSpec
 import fuzs.multiloader.neoforge.toml.NeoForgeModsTomlTask
-import mod
 import net.fabricmc.loom.task.RemapJarTask
 import org.gradle.api.internal.tasks.JvmConstants
-import versionCatalog
+import kotlin.jvm.optionals.getOrNull
 
 plugins {
     id("fuzs.multiloader.multiloader-convention-plugins-platform")
@@ -49,9 +47,6 @@ dependencies {
     add("commonJava", project(mapOf("path" to ":Common-NeoForgeApi", "configuration" to "commonJava")))
     add("commonResources", project(mapOf("path" to ":Common-NeoForgeApi", "configuration" to "commonResources")))
     "forge"(versionCatalog.findLibrary("minecraftforge.forge").get())
-    api(libs.mixinextras.common)
-    annotationProcessor(libs.mixinextras.common)
-    include(libs.mixinextras.forge)
 }
 
 tasks.named<RemapJarTask>("remapJar") {
@@ -59,7 +54,25 @@ tasks.named<RemapJarTask>("remapJar") {
 }
 
 val generateModsToml = tasks.register<NeoForgeModsTomlTask>("generateModsToml") {
+    fun version(alias: String): String? =
+        project.versionCatalog.findVersion(alias).getOrNull()?.requiredVersion?.let { "[${it},)" }
+
     setupModsTomlTask()
+    outputFile.set(project.layout.buildDirectory.file("generated/resources/META-INF/mods.toml"))
+
+    toml {
+        dependencies.set(
+            dependencies.get().filterNot {
+                it.properties.get().modId.orNull == "neoforge"
+            }
+        )
+
+        dependency(project.mod.id) {
+            modId.set("forge")
+            type.set(NeoForgeModsTomlSpec.DependencySpec.Type.REQUIRED)
+            version("minecraftforge.min")?.let { versionRange.set(it) }
+        }
+    }
 }
 
 tasks.named<ProcessResources>(JvmConstants.PROCESS_RESOURCES_TASK_NAME) {
